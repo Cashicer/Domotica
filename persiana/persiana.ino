e725f3d0570969275f19734870cd1ee742be5ee5
Cashicer <javier_robi@hotmail.com>
2019-02-12 00:38:19 +0100

    Update persiana.ino
    Configurados pines y setup()

--- persiana/persiana.ino
+++ persiana/persiana.ino
@@ -5,29 +5,47 @@
 */
 
 // Definicion de variables y pines asociados
-int botUp;
-int botDown;
-int botStop;
-int relUp;
-int relDown;
+int botUp = 7;
+int botDown = 5;
+int botStop = 6;
+int relUp = 11;
+int relDown = 12;
 
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(botUp, INPUT);
+  pinMode(botDown, INPUT);
+  pinMode(botStop, INPUT);
+  pinMode(relUp, OUTPUT);
+  pinMode(relDown, OUTPUT);
+  digitalWrite(relUp, HIGH);
+  digitalWrite(relDown, HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (digitalRead(botUp) != 1){
-    digitalWrite(relDown, 0);
-    digitalWrite(relUp, 1);
+  
+  if (digitalRead(botUp) == LOW){
+    while (digitalRead(botUp) == LOW){}
+    digitalWrite(relDown, HIGH);
+    digitalWrite(relUp, LOW);
   }
-  else if (digitalRead(botDown) != 1){
-    digitalWrite(relUp, 0);
-    digitalWrite(relDown, 1);
+  else if (digitalRead(botDown) == LOW){
+    while (digitalRead(botDown) == LOW){}
+    digitalWrite(relUp, HIGH);
+    digitalWrite(relDown, LOW);
   }
-  else if (digitalRead(botStop) != 1){
-    digitalWrite(relUp, 0);
-    digitalWrite(relDown, 0);
+  else if (digitalRead(botStop) == LOW){
+    while (digitalRead(botStop) == LOW){}
+    digitalWrite(relUp, HIGH);
+    digitalWrite(relDown, HIGH);
   }
+  /*
+  digitalWrite(relDown, LOW);
+  digitalWrite(relUp, HIGH);
+  delay(1000);
+  digitalWrite(relUp, LOW);
+  digitalWrite(relDown, HIGH);
+  delay(1000);
+  */
 }
